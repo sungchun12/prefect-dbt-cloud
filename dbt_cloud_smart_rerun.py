ee835f917f9ad0f39b3961516c7546e69a00de13
Sung Won Chung <sungwonchung3@gmail.com>
2022-07-08 09:49:58 -0500

    working examples

--- dbt_cloud_smart_rerun.py
+++ dbt_cloud_smart_rerun.py
@@ -4,26 +4,29 @@ from prefect import flow, task
 from prefect_dbt.cloud import DbtCloudCredentials
 from prefect_dbt.cloud.runs import get_dbt_cloud_run_artifact
 
+# @task
+# def get_artifact_task():
+#     credentials = DbtCloudCredentials(api_key=os.getenv('DBT_TOKEN'), account_id=16173)
+#     artifact = get_dbt_cloud_run_artifact(
+#         dbt_cloud_credentials=credentials,
+#         run_id=66585620,
+#         path="run_results.json"
+#     )
+
+#     return artifact
+  
+@task
+def parse_artifact_task(artifact):
+    print(artifact)
+
 @flow
-def get_artifact_task():
+def smart_rerun_flow():
     credentials = DbtCloudCredentials(api_key=os.getenv('DBT_TOKEN'), account_id=16173)
     artifact = get_dbt_cloud_run_artifact(
         dbt_cloud_credentials=credentials,
         run_id=66585620,
         path="run_results.json"
     )
-
-    return artifact
-  
-@flow
-def parse_artifact_task(artifact):
-    artifact_result = artifact.result()
-    print(artifact_result)
-    return artifact_result
-
-@flow
-def smart_rerun_flow():
-    artifact = get_artifact_task()
     parse_artifact_task(artifact)
 
 smart_rerun_flow()
